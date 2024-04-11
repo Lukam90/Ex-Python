fae6613d51e5220002986440c703c9b6645cf87b
Lucien HAMM <lucien.hamm@gmail.com>
2024-04-11 16:24:15 +0200

    MAJ Ex Kivy > GUI

--- kivy-tasks-app/main.py
+++ kivy-tasks-app/main.py
@@ -2,6 +2,8 @@ from kivymd.app import MDApp
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.pickers import MDDatePicker
+from kivymd.uix.list import TwoLineAvatarIconListItem, ILeftBody
+from kivymd.uix.selectioncontrol import MDCheckbox
 
 from datetime import datetime
 
@@ -14,11 +16,28 @@ class DialogContent(MDBoxLayout):
     def show_date_picker(self):
         date_dialog = MDDatePicker()
         date_dialog.bind(on_save = self.on_save)
+        date_dialog.open()
 
     def on_save(self, instance, value, date_range):
         date = value.strftime("%A %d %B %Y")
 
         self.ids.date_text.text = str(date)
+
+class ListItemWithCheckbox(TwoLineAvatarIconListItem):
+    def __init__(self, pk = None, **kwargs):
+        super().__init__(**kwargs)
+
+        self.pk = pk
+
+    def mark(self, check, the_list_item):
+        if check.active:
+            the_list_item.text = "[s]" + the_list_item.text + "[/s]"
+
+    def delete_item(self, the_list_item):
+        self.parent.remove_widget(the_list_item)
+
+class LeftCheckbox(ILeftBody, MDCheckbox):
+    pass
 
 class MainApp(MDApp):
     task_list_dialog = None
@@ -38,7 +57,11 @@ class MainApp(MDApp):
 
     def add_task(self, task, task_date):
         print(task.text, task_date)
-    
+
+        self.root.ids["container"].add_widget(ListItemWithCheckbox(text = "[b]" + task.text + "[/b]", secondary_text = task_date))
+
+        task.text = ""
+
     def close_dialog(self, *args):
         self.task_list_dialog.dismiss()
 
